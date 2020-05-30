4ca19221b1a596e43a8557140f1a640e12be2ae4
Gabriel Villegas <nspooky@outlook.com>
2020-05-30 04:01:37 -0500

    Polimorfismo en Java

--- Java/Main.java
+++ Java/Main.java
@@ -1,13 +1,12 @@
 class Main {
     public static void main(String[] args) {
-        // System.out.println("J");
-
-        Car car = new Car("ABC-123", new Account("Gabriel Villegas", "INE"));
-        // car.license = "ABC-123";
-        // car.driver = "Gabriel Villegas";
-        // car.passenger = 4;
-        // System.out.println("Car License: " + car.license);
-        car.printDataCar();
+        System.out.println("Hola mundo java in VS Code");
+        UberX uberX = new UberX("ABC-123 ", new Account("Gabriel Villegas ", "Credencial "), "Chevrolet ", "Chayanne ");
+        uberX.setPassenger(4);
+        uberX.printDataCar();
+        UberVan uberVan = new UberVan("DFG-456 ", new Account("Gerardo Villegas ", "Pasaporte "));
+        uberVan.setPassenger(6);
+        uberVan.printDataCar();
 
     }
 }
